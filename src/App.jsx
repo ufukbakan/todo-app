8b596be4ae632b9140c69a54cb7a1a6726a3643c
ufuk bakan <println.ufukbakan@gmail.com>
2021-08-25 22:23:34 +0300

    memory added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import ToDoBlock from './components/ToDoBlock';
 import './main.css'
 
@@ -7,6 +7,32 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { notes: [], notePlaceholder: "", openInput: false, formKey: -1 };
+  }
+
+  createElementFromHTML(htmlString) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+    return div.firstChild;
+  }
+
+  toToDoBlock(string, key) {
+    let oldBlock = this.createElementFromHTML(string);
+    return <ToDoBlock key={key} done={oldBlock.classList.contains("done")} handleDelete={this.removeNote.bind(this, key)}>{oldBlock.querySelector(".description").innerHTML}</ToDoBlock>
+  }
+
+  componentDidMount() {
+    window.addEventListener("keyup", (e) => { if (e.keyCode == 13) this.storeNotes(); });
+    let savedData = localStorage.getItem("notes");
+    if (savedData) {
+      let parsed = JSON.parse(savedData);
+      let key = Date.now();
+      let addNotes = [];
+      parsed.forEach(note => {
+        addNotes.push(this.toToDoBlock(note, key));
+        key += 1;
+      })
+      this.setState({ notes: [...this.state.notes, ...addNotes] });
+    }
   }
 
   async addNote() {
@@ -19,6 +45,7 @@ class App extends Component {
     let notesClone = [...this.state.notes];
     notesClone = notesClone.filter(note => note.key != key);
     await this.setState({ notes: notesClone });
+    this.storeNotes();
   }
 
   handleTextAreaOnChange(e) {
@@ -36,21 +63,20 @@ class App extends Component {
     this.toggleInputForm();
   }
 
-  formInputDone(multi=false) {
+  async formInputDone(multi = false) {
     if (this.state.formKey <= -1) {
       let key = Date.now();
-      if(multi)
-      {
+      if (multi) {
         let values = document.querySelector("#new-note-value").value.split('\n');
         let addNotes = [];
-        values.forEach(async (value)=>{
+        values.forEach(async (value) => {
           addNotes.push(<ToDoBlock key={key} handleDelete={this.removeNote.bind(this, key)} >{value}</ToDoBlock>);
           key += 1;
         });
-        this.setState({ notes: [...this.state.notes, ...addNotes] });
+        await this.setState({ notes: [...this.state.notes, ...addNotes] });
       }
-      else{
-        this.setState({ notes: [...this.state.notes, <ToDoBlock key={key} handleDelete={this.removeNote.bind(this, key)} >{document.querySelector("#new-note-value").value}</ToDoBlock>] });
+      else {
+        await this.setState({ notes: [...this.state.notes, <ToDoBlock key={key} handleDelete={this.removeNote.bind(this, key)} >{document.querySelector("#new-note-value").value}</ToDoBlock>] });
       }
     }
     else {
@@ -60,9 +86,20 @@ class App extends Component {
       let editedNote = <ToDoBlock key={key} done={noteToEdit.props.done} handleDelete={noteToEdit.props.handleDelete} >{document.querySelector("#new-note-value").value}</ToDoBlock>;
       let notesClone = [...this.state.notes];
       notesClone.splice(index, 1, editedNote);
-      this.setState({ notes: notesClone });
+      await this.setState({ notes: notesClone });
     }
     this.toggleInputForm();
+    this.storeNotes();
+  }
+
+  async storeNotes() {
+    await setTimeout(() => {
+      let outerHTMLs = [];
+      document.querySelectorAll(".todo-wrapper").forEach(note => {
+        outerHTMLs.push(note.outerHTML);
+      });
+      localStorage.setItem("notes", JSON.stringify(outerHTMLs));
+    }, 100);
   }
 
   renderNotes() {
@@ -76,7 +113,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="content">
+      <div className="content" onClick={this.storeNotes.bind(this)}>
         <div className="add-button" onClick={this.addNote.bind(this)}><span>+</span></div>
         <div className={!this.state.openInput ? "todo-form hidden" : "todo-form"}>
           <div className="absolute-center boxed center-text">
@@ -87,21 +124,23 @@ class App extends Component {
               <center className="center-text"><label>Her satırı ayrı bir kayıt yap<input id="seperate-lines" type="checkbox"></input></label></center>
               <div className="center-text">
                 <a onClick={() => this.toggleInputForm()} className="button red">İptal</a>
-                <a onClick={() =>{
+                <a onClick={() => {
                   let form = document.querySelector("form");
-                  if(!form.checkValidity()){
+                  if (!form.checkValidity()) {
                     form.querySelector(".hidden-submit").click();
                   }
-                  else{
+                  else {
                     this.formInputDone(document.querySelector("#seperate-lines").checked);
                   }
                 }} className="button">Onayla</a>
-                <input className="hidden-submit" type="submit" style={{display: "none"}}></input>
+                <input className="hidden-submit" type="submit" style={{ display: "none" }}></input>
               </div>
             </form>
           </div>
         </div>
-        {this.renderNotes()}
+        <div className="rendered-notes">
+          {this.renderNotes()}
+        </div>
       </div>
     );
   }
